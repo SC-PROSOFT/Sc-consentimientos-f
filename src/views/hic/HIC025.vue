d9cbcb0f607ee78f58a5190880d18d14c94d2084
JoanQ07 <80782653+JoanQ07@users.noreply.github.com>
2023-11-01 17:17:46 -0500

    Se agrega formato HIC025 al route

---
+++ src/views/hic/HIC025.vue
@@ -0,0 +1,127 @@
+<template>
+  <q-card class="q-ma-xl">
+    <q-card-section>
+      <div class="row">
+        <p>Historia clínica numero:</p>
+        <q-input type="text" dense v-model="hc" class="col-1" />
+      </div>
+      <div class="row">
+        <p>Ciudad:</p>
+        <q-input type="text" dense class="col-1" />
+        <p>fecha:</p>
+        <q-input type="text" dense class="col-1" />
+      </div>
+
+      <div class="row">
+        <p>Yo,</p>
+        <q-input type="text" dense />
+        <p>, identificado (a) con cedula numero</p>
+        <q-input type="text" dense class="col-1" />
+        <p>expedida en</p>
+        <q-input type="text" dense class="col-1" />
+        <p>actuando en nombre propio o como acudiente de</p>
+        <q-input type="text" dense class="col-1" />
+        <p>.</p>
+        <p>
+          Comprendo que durante el procedimiento pueden aparecer circunstancias imprevisibles o inesperadas,
+          que pueden requerir una extensión de otro procedimiento; acepto que las ciencias de la salud no son
+          una ciencia exacta, que se garantizan resultados en la atención, y que aunque son procedimientos
+          seguros pueden presentarse complicaciones como:
+        </p>
+        <q-input
+          placeholder="Ingrese complicaciones"
+          maxlength="200"
+          type="textarea"
+          class="col-12"
+          autogrow
+          dense
+        />
+      </div>
+    </q-card-section>
+    <q-card-section>
+      <p>
+        Me han explicado también que de negarme a realizarme los exámenes diagnósticos, procedimientos y/o
+        tratamientos ordenados, estoy asumiendo la responsabilidad por sus consecuencias, con lo que exonero
+        de ellas el quipo asistencial tratante y la institución, sin embargo ello no significa que pierda mis
+        derechos para una atención posterior.
+      </p>
+      <p>
+        Se me ha informado que en la ESE salud Yopal, cuenta con personal idóneo, competente y capacitado para
+        la determinación de conductas terapéuticas que contribuyan a mejorar mi calidad de vida y salud. Doy
+        constancia de que se me ha explicado en lenguaje sencillo claro, y entendible para mí, los aspectos
+        relacionados con mi condición actual, los riesgos y beneficios de los procedimientos; se me ha
+        permitido hacer todas las preguntas necesarias, y han sido resueltas satisfactoriamente.
+      </p>
+      <p>
+        Por lo tanto, en forma consciente y voluntaria, sin haber sido objeto de coacción, persuasión, ni
+        manipulación:
+      </p>
+      <div class="row">
+        <q-checkbox v-model="reg_hic24.autorizo" color="teal" />
+        <p>
+          <ins class="text-bold">Autorizo</ins> al personal asistencial de la ESE Salud Yopal, para la
+          realización de los procedimientos de salud:
+        </p>
+      </div>
+      <div class="row">
+        <q-checkbox v-model="reg_hic24.revocar" color="teal" />
+        <p>
+          Expreso mi voluntad de <ins class="text-bold">revocar</ins> el consentimiento presentado y declaro
+          por tanto que, tras la información recibida, no consiento someterme al procedimiento de:
+        </p>
+      </div>
+      <div class="row">
+        <q-input type="text" dense class="col-2" />
+        <p>por los siguientes motivos:</p>
+        <q-input type="text" dense class="col-8" />
+      </div>
+    </q-card-section>
+    <q-separator />
+    <q-card-actions align="around" class="row">
+      <ContainerFirma
+        @reciFirma="callBackFirma"
+        firmador="Joan Sebastian Quintero Nieto"
+        quien_firma="FIRMA TUTOR O FAMILIAR"
+        class="col-3"
+      />
+      <ContainerFirma
+        @reciFirma="callBackFirma"
+        firmador="Fernanda Quintero"
+        quien_firma="FIRMA PACIENTE"
+        class="col-3"
+      />
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script setup>
+import { ref, reactive, defineAsyncComponent } from "vue";
+
+const ContainerFirma = defineAsyncComponent(() => import("../../components/global/containerFirma.vue"));
+
+const firma_recibida = ref("");
+const hc = ref("01516116112");
+const reg_hic24 = reactive({
+  usuario_acudir: "",
+  complicaciones: "",
+  procedimiento: "",
+  autorizo: false,
+  revocar: false,
+  exp_ciudad: "",
+  usuario: "",
+  motivos: "",
+  cedula: "",
+});
+
+function callBackFirma(dataF) {
+  firma_recibida.value = dataF;
+}
+</script>
+
+<style>
+p {
+  margin-top: 10px;
+  margin-left: 5px;
+  margin-right: 8px;
+}
+</style>
