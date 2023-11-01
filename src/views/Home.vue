21034d3b0feb55a9c8d3a7cdc2e90a3cc24b7aa3
JoanQ07 <80782653+JoanQ07@users.noreply.github.com>
2023-11-01 08:55:57 -0500

    Se integra HeaderFormat para formatos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="background-container">
     <router-view />
+    <HeaderFormat v-show="route.name != 'menu'" />
   </div>
 </template>
-<script setup></script>
+
+<script setup>
+const HeaderFormat = defineAsyncComponent(() => import("../components/global/headerFormat.vue"));
+import { defineAsyncComponent, ref } from "vue";
+import { useRoute } from "vue-router";
+const route = ref(useRoute());
+</script>
+
 <style>
 .background-container {
   background-size: cover;
