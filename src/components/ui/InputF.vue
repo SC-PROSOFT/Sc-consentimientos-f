f0ccc6dd1139533a2df0828b2ed30e5f4126649c
JoanQ07 <80782653+JoanQ07@users.noreply.github.com>
2023-11-03 18:04:52 -0500

    Se crea InputF

---
+++ src/components/ui/InputF.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-input
+    :style="'min-width:' + props.width + 'px' + ';display:' + props.display"
+    :readonly="props.readonly"
+    :disable="props.disable"
+    :placeholder="placeholder"
+    :type="props.tipo"
+    v-model="value"
+    dense
+  />
+</template>
+
+<script setup>
+import { onMounted, computed, ref } from "vue";
+
+const emit = defineEmits(["update:modelValue"]);
+const props = defineProps({
+  modelValue: "",
+  tipo: {
+    type: String,
+    default: "text",
+  },
+  readonly: {
+    type: Boolean,
+    default: false,
+  },
+  disable: {
+    type: Boolean,
+    default: false,
+  },
+  width: {
+    type: String,
+    default: "100px",
+  },
+  display: {
+    type: String,
+    default: "inline-block",
+  },
+  placeholder: {
+    type: String,
+    default: "",
+  },
+});
+
+const value = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit("update:modelValue", value);
+  },
+});
+</script>
--- src/setup/components.js
+++ src/setup/components.js
@@ -2,6 +2,7 @@
 // export const componente = defineAsyncComponent(() => import("@/components/UI/componente.vue"));
 
 import Input_ from "@/components/UI/Input.vue";
+import InputF_ from "@/components/UI/InputF.vue";
 import Toggle_ from "@/components/UI/Toggle.vue";
 import Select_ from "@/components/UI/Select.vue";
 import Header_ from "@/components/UI/Header.vue";
@@ -12,6 +13,7 @@ import Autocomplete_ from "@/components/UI/Autocomplete.vue";
 
 export default function registerComponents(app) {
   app.component("Input_", Input_);
+  app.component("InputF_", InputF_);
   app.component("Toggle_", Toggle_);
   app.component("Header_", Header_);
   app.component("Select_", Select_);
