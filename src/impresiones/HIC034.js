878fd8f13d779ac5194c07fbc33edbb4ec8f9853
JoanQ07 <80782653+JoanQ07@users.noreply.github.com>
2023-11-09 14:04:25 -0500

    Cambios HIC034

--- src/impresiones/HIC034.js
+++ src/impresiones/HIC034.js
@@ -1,7 +1,5 @@
 import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
 
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
 export const impresionHC034 = () => {
   return new Promise(async (resolve) => {
     try {
