5885f3b8d30e8930a2391808b174a6a911df29c4
Cristian David Hernandez Suarez <pastusito_14@hotmail.com>
2023-11-14 17:52:30 -0500

    [Fix] - configuracion vite config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
     vue({
       template: { transformAssetUrls },
     }),
-
     quasar({ sassVariables: "src/assets/styles/quasar-variables.sass" }),
   ],
   define: {
