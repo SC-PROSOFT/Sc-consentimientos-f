55f5b84f885f3a0b702a626cb657bcf2b32de71b
Santiago Lozada <100322634+MrCouishle@users.noreply.github.com>
2023-11-15 11:56:54 -0500

    [Fix] Se añaden cambios en huella y firmas desde HIC031 - HIC042

--- src/impresiones/HIC041.js
+++ src/impresiones/HIC041.js
@@ -2,7 +2,6 @@ import { evaluarParentesco } from "@/formatos/utils";
 import dayjs from "dayjs";
 
 export const impresionHC041 = ({ datos }) => {
-  console.log("⚡  file: HIC041.js:5  datos-->", datos)
   var dd = {
     stack: [contenidOingUrgerncias(), firmas()],
   };
@@ -175,7 +174,48 @@ export const impresionHC041 = ({ datos }) => {
     ];
   }
 
-  function firmaPaciente() {
+  function firmaHuellaPaci(huella_paci, cant_firmas) {
+    let tamano_firma = 0;
+
+    if (cant_firmas == 2) {
+      tamano_firma = 105;
+    } else {
+      tamano_firma = 130;
+    }
+    const conHuella = {
+      marginLeft: 3,
+      columns: [
+        {
+          marginTop: 9,
+          alignment: "center",
+          image: "firma_paci",
+          width: tamano_firma,
+          height: 70,
+        },
+        {
+          marginTop: 9,
+          marginLeft: 5,
+          image: "huella_paci",
+          width: 55,
+          height: 70,
+        },
+      ],
+    };
+
+    const sinHuella = {
+      marginLeft: 3,
+      marginTop: 9,
+      alignment: "center",
+      image: "firma_paci",
+      width: tamano_firma,
+      height: 70,
+    };
+
+    if (huella_paci) return conHuella;
+    else return sinHuella;
+  }
+
+  function firmaPaciente(huella_paci, cant_firmas) {
     return {
       stack: [
         {
@@ -184,24 +224,7 @@ export const impresionHC041 = ({ datos }) => {
           alignment: "center",
           style: "tableNoBold",
         },
-        {
-          columns: [
-            {
-              marginTop: 9,
-              alignment: "center",
-              image: "firma_paci",
-              width: 105,
-              height: 70,
-            },
-            {
-              marginTop: 9,
-              marginLeft: 8,
-              image: "huella_paci",
-              width: 55,
-              height: 70,
-            },
-          ],
-        },
+        firmaHuellaPaci(huella_paci, cant_firmas),
         {
           marginTop: 10,
           columns: [
@@ -378,17 +401,20 @@ export const impresionHC041 = ({ datos }) => {
   function firmas() {
     let firmasArray = [];
     let anchos = [];
-
-    if (datos.firmas.firma_paci) {
-      firmasArray.push(firmaPaciente());
-    }
-
+    let tamanoFirmasArray = 0;
+    
     if (datos.firmas.firma_acomp) {
       firmasArray.push(firmaAcompanante());
     }
-
+    
     if (datos.firmas.firma_prof) {
       firmasArray.push(firmaProfesional());
+    }
+
+    tamanoFirmasArray = firmasArray.length
+
+    if (datos.firmas.firma_paci) {
+      firmasArray.unshift(firmaPaciente(datos.firmas.huella_paci, tamanoFirmasArray));
     }
 
     if (firmasArray.length == 2) {
