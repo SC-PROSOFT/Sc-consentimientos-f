6fc79cf3b39bd16c13b221701debf827ead2f29b
JoanQ07 <80782653+JoanQ07@users.noreply.github.com>
2023-11-16 15:02:42 -0500

    [Fix] LAB ROUTE

--- src/router/lab.route.js
+++ src/router/lab.route.js
@@ -1,39 +1,52 @@
 export const createRouteLab = (title) => [
-    {
-      path: "/LAB001",
-      name: "LAB001",
-      component: () => import("@/views/lab/LAB001.vue"),
-      meta: {
-        require_auth: true,
-        title: `${title} - FORMATO LAB001`,
-      },
-    }, 
-    {
-      path: "/LAB002",
-      name: "LAB002",
-      component: () => import("@/views/lab/LAB002.vue"),
-      meta: {
-        require_auth: true,
-        title: `${title} - FORMATO LAB002`,
-      },
-    }, 
-    {
-      path: "/LAB003",
-      name: "LAB003",
-      component: () => import("@/views/lab/LAB003.vue"),
-      meta: {
-        require_auth: true,
-        title: `${title} - FORMATO LAB003`,
-      },
-    }, 
-    {
-      path: "/LAB008",
-      name: "LAB008",
-      component: () => import("@/views/lab/LAB008.vue"),
-      meta: {
-        require_auth: true,
-        title: `${title} - FORMATO LAB008`,
-      },
-    }, 
-  ];
-  
+  {
+    path: "/LAB001",
+    name: "LAB001",
+    component: () => import("@/views/lab/LAB001.vue"),
+    meta: {
+      require_auth: true,
+      title: `${title} - FORMATO LAB001`,
+      descrip: "Consentimiento informado imagenologia",
+    },
+  },
+  {
+    path: "/LAB002",
+    name: "LAB002",
+    component: () => import("@/views/lab/LAB002.vue"),
+    meta: {
+      require_auth: true,
+      title: `${title} - FORMATO LAB002`,
+      descrip: "Consentimiento informado Sedacion",
+    },
+  },
+  {
+    path: "/LAB003",
+    name: "LAB003",
+    component: () => import("@/views/lab/LAB003.vue"),
+    meta: {
+      require_auth: true,
+      title: `${title} - FORMATO LAB003`,
+      descrip: "Consentimiento informado radiología convencional",
+    },
+  },
+  {
+    path: "/LAB004",
+    name: "LAB004",
+    component: () => import("@/views/lab/LAB004.vue"),
+    meta: {
+      require_auth: true,
+      title: `${title} - FORMATO LAB004`,
+      descrip: "Consentimiento informado mamografia",
+    },
+  },
+  {
+    path: "/LAB008",
+    name: "LAB008",
+    component: () => import("@/views/lab/LAB008.vue"),
+    meta: {
+      require_auth: true,
+      title: `${title} - FORMATO LAB008`,
+      descrip: "Consentimiento informado biopsia",
+    },
+  },
+];
