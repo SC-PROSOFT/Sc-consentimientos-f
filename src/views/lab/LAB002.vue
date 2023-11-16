00c2ae45c66b11757c87f46e545efbbb3b6f768b
JoanQ07 <80782653+JoanQ07@users.noreply.github.com>
2023-11-16 14:06:17 -0500

    [Feat] Se maqueta LAB002

--- src/views/lab/LAB002.vue
+++ src/views/lab/LAB002.vue
@@ -2,60 +2,164 @@
   <q-card class="q-mx-auto format q-mb-lg" style="overflow: auto">
     <q-card-section>
       <q-form @submit="validarDatos">
+        <div class="text-center">
+          <q-toggle
+            v-model="reg.opcion_lab002"
+            color="primary"
+            keep-color
+            false-value="REVOCAR"
+            true-value="AUTORIZAR"
+            unchecked-icon="block"
+            checked-icon="check_circle"
+            label="¿Autorizar o revocar este consentimiento?"
+          />
+          <p :class="reg.opcion_lab002 == 'AUTORIZAR' ? 'text-green' : 'text-red'">
+            <q-chip
+              :color="reg.opcion_lab002 == 'AUTORIZAR' ? 'green' : 'red'"
+              class="text-white"
+              v-if="reg.opcion_lab002"
+            >
+              {{ reg.opcion_lab002 }}
+            </q-chip>
+          </p>
+        </div>
         <DatosFormat :datos="datos" />
-        <div class="text-center text-subtitle1 text-bold q-py-xs">NORMATIVIDAD VIGENTE</div>
-        <p class="row text-justify">
-          DD MM AAAA EDAD CC CE PA PT RC TI N° 1 2 N° 1 2 La administración de sedación es un proceso complejo
-          y dinámico, que requiere un monitoreo continuo, pues el grado de sedación al que se somete el
-          paciente puede cambiar rápidamente. Los medicamentos empleados para sedación se relacionan con
-          eventos adversos, como la hipotensión, la bradicardia, depresión respiratoria, hipoxia, aspiración,
-          laringoespasmo, que fácilmente pueden poner en riesgo la vida. La administración de sedación fuera
-          de la sala de cirugía incrementa el riesgo de evento adverso, por esto, en UTM el personal
-          involucrado en esta práctica tiene las competencias necesarias para garantizar la seguridad del
-          procedimiento, la sedación se encuentra dirigida por el médico anestesiólogo, quien además acompaña
-          al paciente durante el procedimiento y la recuperación. La Ley 23 de 1981 al referirse a las
-          relaciones médico – paciente, en los artículos 14, 15 y 18, advierte la necesidad del consentimiento
-          para realizar los diferentes tratamientos medico quirúrgicos que se requieran. Para la resolución
-          3100 de 2019 el Consentimiento informado es la aceptación libre, voluntaria y consciente de un
-          paciente o usuario, manifestada en el pleno uso de sus facultades, para que tenga a lugar un acto
-          asistencial. Para que el consentimiento se considere informado, el paciente o usuario deberá
-          entender la naturaleza de la decisión a consentir tras recibir información que le haga consciente de
-          los beneficios, riesgos, alternativas e implicaciones del acto asistencial
-        </p>
-        <div class="text-center text-subtitle1 text-bold q-py-xs">INFORMACIÓN DEL PROCEDIMIENTO</div>
-        <p class="row text-justify">
-          La sedación es una práctica médica que se puede considerar como una estrategia para mejorar la
-          calidad del cuidado proporcionado al paciente, llegando incluso a considerarse como un indicador de
-          calidad; se emplea en procedimientos invasivos y no invasivos, de carácter diagnóstico o
-          intervencionista. La sedación consiste en la administración de agentes sedantes o disociativos
-          (medicamentos) para disminuir el dolor, la ansiedad y la incomodidad que pueden producir los
-          procedimientos médicos. La sedación fuera del quirófano hace referencia a la administración de
-          sedación en un entorno diferente a las salas de cirugía, una práctica que puede ser usual en
-          procedimientos de radiología, intervenciones diagnósticas, procedimientos pediátricos o en pacientes
-          psiquiátricos, entre otros.
-        </p>
-        <div class="text-center text-subtitle1 text-bold q-py-xs">BENEFICIOS</div>
-        <p class="row text-justify">
-          Con mucha frecuencia la necesidad de obtener imágenes de alta calidad sólo depende de la inmovilidad
-          del paciente, como es en el caso de la resonancia magnética o la tomografía axial computarizada;
-          estos procedimiento no implicann dolor, pero en situaciones donde el enfermo se encuentra ansioso,
-          sufre de claustrofobia o no puede permanecer quieto, las técnicas de sedación o anestesia en
-          ocasiones son indispensables para proporcionarle comodidad y seguridad al paciente.
-        </p>
-        <div class="text-center text-subtitle1 text-bold q-py-xs">RIESGOS</div>
-        <p class="row text-justify">
-          DD MM AAAA EDAD CC CE PA PT RC TI N° 1 2 N° 1 2 La administración de sedación es un proceso complejo
-          y dinámico, que requiere un monitoreo continuo, pues el grado de sedación al que se somete el
-          paciente puede cambiar rápidamente. Los medicamentos empleados para sedación se relacionan con
-          eventos adversos, como la hipotensión, la bradicardia, depresión respiratoria, hipoxia, aspiración,
-          laringoespasmo, que fácilmente pueden poner en riesgo la vida. La administración de sedación fuera
-          de la sala de cirugía incrementa el riesgo de evento adverso, por esto, en UTM el personal
-          involucrado en esta práctica tiene las competencias necesarias para garantizar la seguridad del
-          procedimiento, la sedación se encuentra dirigida por el médico anestesiólogo, quien además acompaña
-          al paciente durante el procedimiento y la recuperación.
-        </p>
+        <div class="border-format q-my-sm">
+          <div class="text-center text-subtitle1 text-bold q-py-xs">NORMATIVIDAD VIGENTE</div>
+          <p class="row text-justify">
+            DD MM AAAA EDAD CC CE PA PT RC TI N° 1 2 N° 1 2 La administración de sedación es un proceso
+            complejo y dinámico, que requiere un monitoreo continuo, pues el grado de sedación al que se
+            somete el paciente puede cambiar rápidamente. Los medicamentos empleados para sedación se
+            relacionan con eventos adversos, como la hipotensión, la bradicardia, depresión respiratoria,
+            hipoxia, aspiración, laringoespasmo, que fácilmente pueden poner en riesgo la vida. La
+            administración de sedación fuera de la sala de cirugía incrementa el riesgo de evento adverso, por
+            esto, en UTM el personal involucrado en esta práctica tiene las competencias necesarias para
+            garantizar la seguridad del procedimiento, la sedación se encuentra dirigida por el médico
+            anestesiólogo, quien además acompaña al paciente durante el procedimiento y la recuperación. La
+            Ley 23 de 1981 al referirse a las relaciones médico – paciente, en los artículos 14, 15 y 18,
+            advierte la necesidad del consentimiento para realizar los diferentes tratamientos medico
+            quirúrgicos que se requieran. Para la resolución 3100 de 2019 el Consentimiento informado es la
+            aceptación libre, voluntaria y consciente de un paciente o usuario, manifestada en el pleno uso de
+            sus facultades, para que tenga a lugar un acto asistencial. Para que el consentimiento se
+            considere informado, el paciente o usuario deberá entender la naturaleza de la decisión a
+            consentir tras recibir información que le haga consciente de los beneficios, riesgos, alternativas
+            e implicaciones del acto asistencial
+          </p>
+        </div>
+        <div class="border-format q-my-sm">
+          <div class="text-center text-subtitle1 text-bold q-py-xs">INFORMACIÓN DEL PROCEDIMIENTO</div>
+          <p class="row text-justify">
+            La sedación es una práctica médica que se puede considerar como una estrategia para mejorar la
+            calidad del cuidado proporcionado al paciente, llegando incluso a considerarse como un indicador
+            de calidad; se emplea en procedimientos invasivos y no invasivos, de carácter diagnóstico o
+            intervencionista. La sedación consiste en la administración de agentes sedantes o disociativos
+            (medicamentos) para disminuir el dolor, la ansiedad y la incomodidad que pueden producir los
+            procedimientos médicos. La sedación fuera del quirófano hace referencia a la administración de
+            sedación en un entorno diferente a las salas de cirugía, una práctica que puede ser usual en
+            procedimientos de radiología, intervenciones diagnósticas, procedimientos pediátricos o en
+            pacientes psiquiátricos, entre otros.
+          </p>
+        </div>
+        <div class="border-format q-my-sm">
+          <div class="text-center text-subtitle1 text-bold q-py-xs">BENEFICIOS</div>
+          <p class="row text-justify">
+            Con mucha frecuencia la necesidad de obtener imágenes de alta calidad sólo depende de la
+            inmovilidad del paciente, como es en el caso de la resonancia magnética o la tomografía axial
+            computarizada; estos procedimiento no implicann dolor, pero en situaciones donde el enfermo se
+            encuentra ansioso, sufre de claustrofobia o no puede permanecer quieto, las técnicas de sedación o
+            anestesia en ocasiones son indispensables para proporcionarle comodidad y seguridad al paciente.
+          </p>
+        </div>
+        <div class="border-format q-my-sm">
+          <div class="text-center text-subtitle1 text-bold q-py-xs">RIESGOS</div>
+          <p class="row text-justify">
+            DD MM AAAA EDAD CC CE PA PT RC TI N° 1 2 N° 1 2 La administración de sedación es un proceso
+            complejo y dinámico, que requiere un monitoreo continuo, pues el grado de sedación al que se
+            somete el paciente puede cambiar rápidamente. Los medicamentos empleados para sedación se
+            relacionan con eventos adversos, como la hipotensión, la bradicardia, depresión respiratoria,
+            hipoxia, aspiración, laringoespasmo, que fácilmente pueden poner en riesgo la vida. La
+            administración de sedación fuera de la sala de cirugía incrementa el riesgo de evento adverso, por
+            esto, en UTM el personal involucrado en esta práctica tiene las competencias necesarias para
+            garantizar la seguridad del procedimiento, la sedación se encuentra dirigida por el médico
+            anestesiólogo, quien además acompaña al paciente durante el procedimiento y la recuperación.
+          </p>
+        </div>
+        <div class="border-format q-my-sm" v-if="reg.opcion_lab002 == 'AUTORIZAR'">
+          <div class="text-center text-subtitle1 text-bold q-py-xs">
+            DECLARACION DEL CONSENTIMIENTO INFORMADO
+          </div>
+          <p class="row text-justify">
+            Si ha comprendido la información contenida en el presente documento y acepta voluntariamente la
+            realización del procedimiento en mención, proceda a firmar dejando su autorización por escrito
+          </p>
+          <p>
+            Yo <InputF_ v-model="getPaci.descrip" width="300" />, identificada (o) con el documento de
+            identidad número <InputF_ v-model="getPaci.cod" />, después de haber sido informado (a) sobre el
+            procedimiento de Sedación, los riesgos y beneficios, declaro que la información ha sido clara, que
+            se me han respondido las inquietudes y que autorizo la toma del procedimiento teniendo en cuenta
+            que esta autorización puede ser revocable en cualquier momento.
+          </p>
+        </div>
+        <div class="border-format q-my-sm" v-if="reg.opcion_lab002 == 'REVOCAR'">
+          <div class="text-center text-subtitle1 text-bold q-py-xs">
+            DECLARACION DEL CONSENTIMIENTO INFORMADO
+          </div>
+          <p class="row text-justify">
+            Si ha comprendido la información contenida en el presente documento y acepta voluntariamente la
+            realización del procedimiento en mención, proceda a firmar dejando su autorización por escrito
+          </p>
+          <p>
+            Yo <InputF_ v-model="getPaci.descrip" width="300" />, identificada (o) con el documento de
+            identidad número <InputF_ v-model="getPaci.cod" />, después de haber sido informado (a) sobre el
+            procedimiento de Sedación, sus riesgos y beneficios y adicionalmente, los riesgos de no
+            realizármelo, declaro que la información ha sido clara, que se me han respondido las inquietudes y
+            que autorizo de forma libre y consiente, revoco mi consentimiento para continuar con la toma del
+            procedimiento en mención.
+          </p>
+        </div>
       </q-form>
     </q-card-section>
+    <q-card-actions>
+      <div class="col-12 row justify-around">
+        <ContainerFirma
+          quien_firma="FIRMA PACIENTE"
+          :firmador="getPaci.descrip"
+          :registro_profe="getPaci.cod"
+          @reciFirma="callBackFirma"
+          :huella_="huella_paci"
+          class="col-4"
+        />
+
+        <ContainerFirma
+          :firmador="getAcomp.descrip || 'NO HAY ACOMPAÑANTE'"
+          :disable="!getAcomp.descrip ? true : false"
+          @reciFirma="callBackFirmaAcomp"
+          :registro_profe="getAcomp.cod"
+          quien_firma="TESTIGO"
+          class="col-4"
+        />
+        <ContainerFirma
+          @reciFirma="callBackFirma"
+          :firma_="firma_prof"
+          :firmador="getProf.descrip"
+          :descrip_prof="getProf.descrip_atiende"
+          :registro_profe="getProf.registro_profe"
+          quien_firma="FIRMA PROFESIONAL"
+          class="col-4"
+        />
+      </div>
+      <div class="col-12 row justify-center q-my-md">
+        <q-btn
+          :disable="reg.opcion_lab002 ? false : true"
+          @click="validarDatos"
+          icon-right="check_circle"
+          class="q-mr-lg"
+          color="green"
+          label="GRABAR"
+          type="submit"
+        />
+      </div>
+    </q-card-actions>
   </q-card>
 </template>
 
@@ -76,12 +180,18 @@ const { getPaci, getAcomp, getHc, getProf, getEmpresa, getSesion } = useModuleFo
 const { CON851P } = useModuleCon851p();
 const { CON851 } = useModuleCon851();
 
+const firma_recibida_acomp = ref("");
+const firma_recibida = ref("");
+const huella_paci = ref(null);
+const firma_prof = ref(null);
+
 const datos = {
   tipo_id: getPaci.tipo_id,
-  active_cups: true,
+  active_cups: false,
 };
 
 const reg = ref({
+  opcion_lab002: "",
   fecha_act: "",
   edad: "",
 });
@@ -91,7 +201,15 @@ onMounted(() => {
   reg.value.edad = calcEdad(getPaci.nacim);
 });
 
-function validarDatos() {}
+const validarDatos = () => {};
+
+const callBackFirma = (data_firma) => {
+  data_firma && (firma_recibida.value = data_firma.slice(22));
+};
+
+const callBackFirmaAcomp = (data_firma) => {
+  data_firma && (firma_recibida_acomp.value = data_firma.slice(22));
+};
 </script>
 
 <style>
