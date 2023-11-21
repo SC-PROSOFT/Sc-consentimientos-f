a9ac863d5c8343fe8341059a138f947b8289ca37
Cristian David Hernandez Suarez <pastusito_14@hotmail.com>
2023-11-21 17:19:20 -0500

    [Fix] - Configuracion de apuntador ip

---
+++ src/views/lab/LAB001.vue
@@ -0,0 +1,208 @@
+<template>
+  <q-card class="q-mx-auto format q-mb-lg" style="overflow: auto">
+    <q-card-section>
+      <q-form @submit="validarDatos">
+        <div class="text-center">
+          <q-toggle
+            v-model="reg.opcion_lab002"
+            color="primary"
+            keep-color
+            false-value="REVOCAR"
+            true-value="AUTORIZAR"
+            unchecked-icon="block"
+            checked-icon="check_circle"
+            label="¿Autorizar o revocar este consentimiento?"
+          />
+          <p :class="reg.opcion_lab002 == 'AUTORIZAR' ? 'text-green' : 'text-red'">
+            <q-chip
+              :color="reg.opcion_lab002 == 'AUTORIZAR' ? 'green' : 'red'"
+              class="text-white"
+              v-if="reg.opcion_lab002"
+            >
+              {{ reg.opcion_lab002 }}
+            </q-chip>
+          </p>
+        </div>
+        <DatosFormat :datos="datos" @datos="(data) => (datos.servicio = data)" />
+        <div class="border-format q-my-sm">
+          <div class="text-center text-subtitle1 text-bold q-py-xs">NORMATIVIDAD VIGENTE</div>
+          <p class="row text-justify">
+            La Ley 23 de 1981 al referirse a las relaciones médico – paciente, en los artículos 14, 15 y 18,
+            advierte la necesidad del consentimiento para realizar los diferentes tratamientos medico
+            quirúrgicos que se requieran. Para la resolución 3100 de 2019 el Consentimiento informado es la
+            aceptación libre, voluntaria y consciente de un paciente o usuario, manifestada en el pleno uso de
+            sus facultades, para que tenga a lugar un acto asistencial. Para que el consentimiento se
+            considere informado, el paciente o usuario deberá entender la naturaleza de la decisión a
+            consentir tras recibir información que le haga consciente de los beneficios, riesgos, alternativas
+            e implicaciones del acto asistencial
+          </p>
+        </div>
+        <div class="border-format q-my-sm">
+          <div class="text-center text-subtitle1 text-bold q-py-xs">INFORMACIÓN DEL PROCEDIMIENTO</div>
+          <p class="row text-justify">
+            La sedación es una práctica médica que se puede considerar como una estrategia para mejorar la
+            calidad del cuidado proporcionado al paciente, llegando incluso a considerarse como un indicador
+            de calidad; se emplea en procedimientos invasivos y no invasivos, de carácter diagnóstico o
+            intervencionista. La sedación consiste en la administración de agentes sedantes o disociativos
+            (medicamentos) para disminuir el dolor, la ansiedad y la incomodidad que pueden producir los
+            procedimientos médicos. La sedación fuera del quirófano hace referencia a la administración de
+            sedación en un entorno diferente a las salas de cirugía, una práctica que puede ser usual en
+            procedimientos de radiología, intervenciones diagnósticas, procedimientos pediátricos o en
+            pacientes psiquiátricos, entre otros.
+          </p>
+        </div>
+        <div class="border-format q-my-sm">
+          <div class="text-center text-subtitle1 q-py-xs"><ins class="text-bold">BENEFICIOS</ins></div>
+          <p class="row text-justify">
+            Con mucha frecuencia la necesidad de obtener imágenes de alta calidad sólo depende de la
+            inmovilidad del paciente, como es en el caso de la resonancia magnética o la tomografía axial
+            computarizada; estos procedimiento no implicann dolor, pero en situaciones donde el enfermo se
+            encuentra ansioso, sufre de claustrofobia o no puede permanecer quieto, las técnicas de sedación o
+            anestesia en ocasiones son indispensables para proporcionarle comodidad y seguridad al paciente.
+          </p>
+        </div>
+        <div class="border-format q-my-sm">
+          <div class="text-center text-subtitle1 text-bold q-py-xs">RIESGOS</div>
+          <p class="row text-justify">
+            La administración de sedación es un proceso complejo y dinámico, que requiere un monitoreo
+            continuo, pues el grado de sedación al que se somete el paciente puede cambiar rápidamente. Los
+            medicamentos empleados para sedación se relacionan con eventos adversos, como la hipotensión, la
+            bradicardia, depresión respiratoria, hipoxia, aspiración, laringoespasmo, que fácilmente pueden
+            poner en riesgo la vida. La administración de sedación fuera de la sala de cirugía incrementa el
+            riesgo de evento adverso, por esto, en UTM el personal involucrado en esta práctica tiene las
+            competencias necesarias para garantizar la seguridad del procedimiento, la sedación se encuentra
+            dirigida por el médico anestesiólogo, quien además acompaña al paciente durante el procedimiento y
+            la recuperación.
+          </p>
+        </div>
+        <div class="border-format q-my-sm" v-if="reg.opcion_lab002 == 'AUTORIZAR'">
+          <div class="text-center text-subtitle1 text-bold q-py-xs">
+            DECLARACION DEL CONSENTIMIENTO INFORMADO
+          </div>
+          <p class="row text-justify">
+            Si ha comprendido la información contenida en el presente documento y acepta voluntariamente la
+            realización del procedimiento en mención, proceda a firmar dejando su autorización por escrito
+          </p>
+          <p>
+            Yo <InputF_ v-model="getPaci.descrip" width="300" />, identificada (o) con el documento de
+            identidad número <InputF_ v-model="getPaci.cod" />, después de haber sido informado (a) sobre el
+            procedimiento de Sedación, los riesgos y beneficios, declaro que la información ha sido clara, que
+            se me han respondido las inquietudes y que autorizo la toma del procedimiento teniendo en cuenta
+            que esta autorización puede ser revocable en cualquier momento.
+          </p>
+        </div>
+        <div class="border-format q-my-sm" v-if="reg.opcion_lab002 == 'REVOCAR'">
+          <div class="text-center text-subtitle1 text-bold q-py-xs">
+            REVOCACIÓN DEL CONSENTIMIENTO INFORMADO
+          </div>
+          <p>
+            Yo <InputF_ v-model="getPaci.descrip" width="300" />, identificada (o) con el documento de
+            identidad número <InputF_ v-model="getPaci.cod" />, después de haber sido informado (a) sobre el
+            procedimiento de Sedación, sus riesgos y beneficios y adicionalmente, los riesgos de no
+            realizármelo, declaro que la información ha sido clara, que se me han respondido las inquietudes y
+            que autorizo de forma libre y consiente, revoco mi consentimiento para continuar con la toma del
+            procedimiento en mención.
+          </p>
+        </div>
+      </q-form>
+    </q-card-section>
+    <q-card-actions>
+      <div class="col-12 row justify-around">
+        <ContainerFirma
+          quien_firma="FIRMA PACIENTE"
+          :firmador="getPaci.descrip"
+          :registro_profe="getPaci.cod"
+          @reciFirma="callBackFirma"
+          :huella_="huella_paci"
+          class="col-4"
+        />
+
+        <ContainerFirma
+          :firmador="getAcomp.descrip || 'NO HAY ACOMPAÑANTE'"
+          :disable="!getAcomp.descrip ? true : false"
+          @reciFirma="callBackFirmaAcomp"
+          :registro_profe="getAcomp.cod"
+          quien_firma="TESTIGO"
+          class="col-4"
+        />
+        <ContainerFirma
+          @reciFirma="callBackFirma"
+          :firma_="firma_prof"
+          :firmador="getProf.descrip"
+          :descrip_prof="getProf.descrip_atiende"
+          :registro_profe="getProf.registro_profe"
+          quien_firma="FIRMA PROFESIONAL"
+          class="col-4"
+        />
+      </div>
+      <div class="col-12 row justify-center q-my-md">
+        <q-btn
+          :disable="reg.opcion_lab002 ? false : true"
+          @click="validarDatos"
+          icon-right="check_circle"
+          class="q-mr-lg"
+          color="green"
+          label="GRABAR"
+          type="submit"
+        />
+      </div>
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script setup>
+import { useModuleFormatos, useApiContabilidad, useModuleCon851, useModuleCon851p } from "@/store";
+import { impresionHC030, impresion, generarArchivo } from "@/impresiones";
+import { ref, defineAsyncComponent, onMounted, watch } from "vue";
+import { utilsFormat, calcEdad } from "@/formatos/utils";
+import { useRouter } from "vue-router";
+import dayjs from "dayjs";
+
+const ContainerFirma = defineAsyncComponent(() => import("@/components/global/containerFirma.vue"));
+const DatosFormat = defineAsyncComponent(() => import("@/components/global/DatosFormat.vue"));
+const router = useRouter();
+
+const { getDll$, _getFirma$, _getHuella$, guardarFile$, enviarCorreo$, getEncabezado } = useApiContabilidad();
+const { getPaci, getAcomp, getHc, getProf, getEmpresa, getSesion } = useModuleFormatos();
+const { CON851P } = useModuleCon851p();
+const { CON851 } = useModuleCon851();
+
+const firma_recibida_acomp = ref("");
+const firma_recibida = ref("");
+const huella_paci = ref(null);
+const firma_prof = ref(null);
+
+const datos = {
+  tipo_id: getPaci.tipo_id,
+  active_cups: true,
+  servicio: "",
+};
+
+const reg = ref({
+  opcion_lab002: "",
+  fecha_act: "",
+});
+
+onMounted(() => {
+  reg.value.fecha_act = dayjs(getEmpresa.fecha_act).format("YYYY-MM-DD");
+});
+
+const validarDatos = () => {};
+
+const callBackFirma = (data_firma) => {
+  data_firma && (firma_recibida.value = data_firma.slice(22));
+};
+
+const callBackFirmaAcomp = (data_firma) => {
+  data_firma && (firma_recibida_acomp.value = data_firma.slice(22));
+};
+</script>
+
+<style>
+table,
+th,
+td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+</style>
