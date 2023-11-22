cb96aeedf34c5fac8c6d4b075a7188d6b41bdd35
Cristian David Hernandez Suarez <pastusito_14@hotmail.com>
2023-11-22 09:00:37 -0500

    [Fix] - Configuracion ip utm

--- src/fuentes/empresas.js
+++ src/fuentes/empresas.js
@@ -2,13 +2,16 @@ export const empresas = {
   // PROSOFT PRUEBA
   1: {
     ip_servicio: "34.234.185.158",
-    ip_v4: "172.26.4.38",
     disco: "D",
   },
   // HOSPITAL YOPAL
   844003225: {
     ip_servicio: "192.168.200.4",
-    ip_v4: "192.168.200.4",
     disco: "E",
+  },
+  // HOSPITAL UTM
+  900193162: {
+    ip_servicio: "192.168.132.210",
+    disco: "D",
   },
 };
