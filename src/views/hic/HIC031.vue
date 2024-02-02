e6b92848724ab16210e5dab314d988dfa59582f6
JoanQ07 <80782653+JoanQ07@users.noreply.github.com>
2024-02-02 11:53:43 -0500

    [Feat] Se agrega huella a container paciente HIC031

--- src/views/hic/HIC031.vue
+++ src/views/hic/HIC031.vue
@@ -29,48 +29,30 @@
         </div>
         <div class="row">
           <p>Ciudad:</p>
-          <q-input
-            type="text"
-            disable
-            v-model="getEmpresa.ciudad_usuar"
-            dense
-            class="col-2 q-mr-md"
-          />
+          <q-input type="text" disable v-model="getEmpresa.ciudad_usuar" dense class="col-2 q-mr-md" />
           <p>Fecha:</p>
-          <q-input
-            v-model="fecha_act"
-            disable
-            dense
-            type="date"
-            class="col-2"
-          />
+          <q-input v-model="fecha_act" disable dense type="date" class="col-2" />
         </div>
         <h6 class="q-mt-lg" style="margin-bottom: 0">LA COLPOSCOPIA</h6>
         <div class="row">
           <p class="text-justify">
-            Es un examen sencillo del cuello, vagina y vulva, similar a la toma
-            de citología cervicouterina, donde se realiza observación con un
-            microscopio especial que permite visualizar los cambios ocurridos en
-            el cuello del útero. Para ayudarnos durante este examen se utilizan
-            sustancias que facilitan el reconocimiento de estas lesiones. Cuando
-            el ginecólogo visualiza las lesiones sospechosas procede a tomar una
-            muestra pequeña (biopsia) para posteriormente ser analizado por un
-            patólogo. Normalmente puede presentar sangrado y dolor durante el
-            procedimiento
+            Es un examen sencillo del cuello, vagina y vulva, similar a la toma de citología cervicouterina,
+            donde se realiza observación con un microscopio especial que permite visualizar los cambios
+            ocurridos en el cuello del útero. Para ayudarnos durante este examen se utilizan sustancias que
+            facilitan el reconocimiento de estas lesiones. Cuando el ginecólogo visualiza las lesiones
+            sospechosas procede a tomar una muestra pequeña (biopsia) para posteriormente ser analizado por un
+            patólogo. Normalmente puede presentar sangrado y dolor durante el procedimiento
           </p>
         </div>
         <p style="margin: 0; font-weight: bold">Riesgos de la colposcopia:</p>
         <q-list bordered>
           <q-item>
             <q-item-section>
-              <q-item-label
-                >1. Molestias producidas por las soluciones
-                aplicadas.</q-item-label
-              >
+              <q-item-label>1. Molestias producidas por las soluciones aplicadas.</q-item-label>
               <q-item-label>2. Calambres, picadas o leve dolor.</q-item-label>
               <q-item-label
-                >3. Sangrado mínimo durante varios días. Si es moderado se
-                dejará una gasa para retirarla 6 horas después</q-item-label
+                >3. Sangrado mínimo durante varios días. Si es moderado se dejará una gasa para retirarla 6
+                horas después</q-item-label
               >
               <q-item-label>4. Infección</q-item-label>
             </q-item-section>
@@ -78,58 +60,34 @@
         </q-list>
         <div class="row">
           <p>Yo</p>
-          <q-input
-            v-model="getPaci.descrip"
-            disable
-            type="text"
-            dense
-            class="col-4"
-          />
+          <q-input v-model="getPaci.descrip" disable type="text" dense class="col-4" />
           <p>identificado (a) con cédula número</p>
-          <q-input
-            v-model="getPaci.cod"
-            disable
-            type="text"
-            dense
-            class="col-2"
-          />
+          <q-input v-model="getPaci.cod" disable type="text" dense class="col-2" />
           <p>de</p>
-          <q-input
-            v-model="getPaci.descrip_ciudad"
-            disable
-            type="text"
-            dense
-            class="col-2"
-          />
+          <q-input v-model="getPaci.descrip_ciudad" disable type="text" dense class="col-2" />
           <p class="text-justify">
-            en forma voluntaria y en pleno uso de mis facultades mentales y
-            psíquicas sin presión o inducción alguna:
+            en forma voluntaria y en pleno uso de mis facultades mentales y psíquicas sin presión o inducción
+            alguna:
           </p>
           <div v-show="opcion_hc031 == 'AUTORIZAR'">
             <p class="text-justify" style="margin-top: -5px">
-              Doy el consentimiento para que el ginecólogo de la E.S.E salud
-              Yopal - Hospital local de Yopal, realice el procedimiento de
-              colposcopia y biopsia. Acepto sus riesgos e imprevistos. Entiendo
-              lo que he leído, se me ha explicado verbalmente y por escrito
-              acerca del procedimiento, los cuidados que debo tener antes y
-              después de la
-              <span style="font-weight: bold">COLPOSCOPIA</span>, los riesgos
-              justificados y previsibles. También se me ha dado la oportunidad
-              de preguntar y resolver dudas. Entiendo que este procedimiento
-              puede traer efectos adversos, como infección y sangrado, propias
-              del procedimiento que aquí autorizo, los cuales asumo bajo mi
-              responsabilidad.
+              Doy el consentimiento para que el ginecólogo de la E.S.E salud Yopal - Hospital local de Yopal,
+              realice el procedimiento de colposcopia y biopsia. Acepto sus riesgos e imprevistos. Entiendo lo
+              que he leído, se me ha explicado verbalmente y por escrito acerca del procedimiento, los
+              cuidados que debo tener antes y después de la
+              <span style="font-weight: bold">COLPOSCOPIA</span>, los riesgos justificados y previsibles.
+              También se me ha dado la oportunidad de preguntar y resolver dudas. Entiendo que este
+              procedimiento puede traer efectos adversos, como infección y sangrado, propias del procedimiento
+              que aquí autorizo, los cuales asumo bajo mi responsabilidad.
             </p>
           </div>
         </div>
         <div class="row q-mt-md q-mb-md row" v-show="opcion_hc031 == 'REVOCAR'">
           <p>
             Expreso mi voluntad de
-            <span style="font-weight: bold; text-decoration: underline"
-              >revocar</span
-            >
-            el consentimiento presentado y declaro por tanto que, tras la
-            información recibida, no consiento someterme al procedimiento de
+            <span style="font-weight: bold; text-decoration: underline">revocar</span>
+            el consentimiento presentado y declaro por tanto que, tras la información recibida, no consiento
+            someterme al procedimiento de
             <span style="font-weight: bold">COLPOSCOPIA</span>
           </p>
           <p>por los siguientes motivos:</p>
@@ -148,7 +106,9 @@
         <ContainerFirma
           quien_firma="FIRMA PACIENTE"
           :firmador="getPaci.descrip"
+          :registro_profe="getPaci.cod"
           @reciFirma="callBackFirma"
+          :huella_="huella_paci"
           class="col-4"
         />
         <ContainerFirma
@@ -186,12 +146,7 @@
 </template>
 
 <script setup>
-import {
-  useModuleFormatos,
-  useApiContabilidad,
-  useModuleCon851,
-  useModuleCon851p,
-} from "@/store";
+import { useModuleFormatos, useApiContabilidad, useModuleCon851, useModuleCon851p } from "@/store";
 import { ref, defineAsyncComponent, onMounted, watch } from "vue";
 import { utilsFormat } from "@/formatos/utils";
 import { impresionHC031, impresion, generarArchivo } from "@/impresiones";
@@ -200,20 +155,10 @@ import { foco_ } from "@/setup";
 import dayjs from "dayjs";
 
 const router = useRouter();
-const ContainerFirma = defineAsyncComponent(() =>
-  import("@/components/global/containerFirma.vue")
-);
+const ContainerFirma = defineAsyncComponent(() => import("@/components/global/containerFirma.vue"));
 
-const { getPaci, getAcomp, getHc, getProf, getEmpresa, getSesion } =
-  useModuleFormatos();
-const {
-  getDll$,
-  _getFirma$,
-  _getHuella$,
-  guardarFile$,
-  enviarCorreo$,
-  getEncabezado,
-} = useApiContabilidad();
+const { getPaci, getAcomp, getHc, getProf, getEmpresa, getSesion } = useModuleFormatos();
+const { getDll$, _getFirma$, _getHuella$, guardarFile$, enviarCorreo$, getEncabezado } = useApiContabilidad();
 const { CON851 } = useModuleCon851();
 const { CON851P } = useModuleCon851p();
 
@@ -225,7 +170,6 @@ const llave = ref(null);
 const fecha_act = ref(null);
 const firma_prof = ref(null);
 const huella_paci = ref(null);
-const revocar_motivos = ref(null);
 
 const HIC031 = ref({
   revocar_motivos: "",
@@ -273,9 +217,7 @@ const callBackFirmaAcomp = (data_firma) => {
 
 const validarDatos = async () => {
   if (opcion_hc031.value == "REVOCAR" && HIC031.value.revocar_motivos == "") {
-    return CON851("?", "info", "Debe ingresar motivos de revocación", () =>
-      foco_(form, "revocar")
-    );
+    return CON851("?", "info", "Debe ingresar motivos de revocación", () => foco_(form, "revocar"));
   }
   grabarConsentimiento();
 };
@@ -337,9 +279,7 @@ const grabarFirmaConsen = async (llave) => {
       async () => {
         const file = await imprimirConsen();
         if (getPaci.email && !/.+@.+\..+/.test(getPaci.email.toLowerCase())) {
-          return CON851("?", "info", "El correo no es valido", () =>
-            router.back()
-          );
+          return CON851("?", "info", "El correo no es valido", () => router.back());
         }
         const response = await enviarCorreo$({
           cuerpo: `SE ADJUNTA ${getEncabezado.descrip} PARA ${getPaci.descrip} IDENTIDICADO CON ${getPaci.cod}`,
