340892d62331a45d183edd9c68d1862282ea53c3
JoanQ07 <80782653+JoanQ07@users.noreply.github.com>
2024-02-28 14:17:07 -0500

    [Feat] Se agrega Update en HomeConfig

--- src/views/HomeConfigUsunet.vue
+++ src/views/HomeConfigUsunet.vue
@@ -7,17 +7,76 @@
     v-if="config_maestro.estado"
     @cerrar="closePage"
   />
+  <Update_
+    :mensaje="datos_actualizacion.mensaje"
+    v-if="datos_actualizacion.estado"
+    @cancelar="validarAct(1)"
+    @aceptar="validarAct(2)"
+  />
 </template>
 
 <script setup>
-import { defineAsyncComponent, ref } from "vue";
+import { useApiContabilidad, useModuleCon851 } from "@/store";
+import { defineAsyncComponent, onMounted, ref } from "vue";
 
 const ConfigMaestros_ = defineAsyncComponent(() => import("@/components/consen/ConfigMaestros.vue"));
 const ConfigUsunet_ = defineAsyncComponent(() => import("@/components/consen/ConfigUsunet.vue"));
 const ToolBar_ = defineAsyncComponent(() => import("@/components/global/ToolBar.vue"));
+const Update_ = defineAsyncComponent(() => import("@/components/global/Update.vue"));
+
+const { getVersionBuild$, actualizarVersion$ } = useApiContabilidad();
+const { CON851 } = useModuleCon851();
 
 const config_maestro = ref({ estado: false });
-const estado = ref(true);
+const datos_actualizacion = ref({
+  estado: false,
+  mensaje: "",
+});
+const estado = ref(false);
+
+onMounted(() => getVersionBuild());
+
+const getVersionBuild = async () => {
+  try {
+    const response = await getVersionBuild$({});
+    datos_actualizacion.value.estado = true;
+    datos_actualizacion.value.mensaje = `La nueva versión ${response} de consentimientos ya esta disponible.
+    ¿Desea actualizarla?`;
+  } catch (error) {
+    CON851("?", "info", error);
+  }
+};
+
+const validarAct = async (opc) => {
+  try {
+    switch (opc) {
+      case 1:
+        return controlarVentanas();
+      case 2:
+        const data = await actualizarVersion();
+        return CON851("?", "success", data, controlarVentanas);
+    }
+  } catch (error) {
+    CON851("?", "info", error);
+  }
+};
+
+const actualizarVersion = async () => {
+  try {
+    const response = await actualizarVersion$({});
+    datos_actualizacion.value.mensaje = "";
+    return response;
+  } catch (error) {
+    throw error;
+  } finally {
+    datos_actualizacion.value.estado = false;
+  }
+};
+
+const controlarVentanas = () => {
+  datos_actualizacion.value.estado = false;
+  estado.value = true;
+};
 
 const closePage = () => {
   setTimeout(() => {
