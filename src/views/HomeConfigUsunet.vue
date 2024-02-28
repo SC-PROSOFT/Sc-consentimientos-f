9174d0ddfbcae929b4951ddce4f5acf8bcea9a16
JoanQ07 <80782653+JoanQ07@users.noreply.github.com>
2024-02-28 10:34:16 -0500

    [Feat] Se integran maestro consen en HomeConfig

--- src/views/HomeConfigUsunet.vue
+++ src/views/HomeConfigUsunet.vue
@@ -1,16 +1,38 @@
 <template>
-  <ConfigUsunet_ :configuracion="{ estado: true }" @cerrar="logOut$" @guardar="closePage" />
+  <ConfigUsunet_ :configuracion="{ estado }" @cerrar="closePage" @guardar="closePage" />
+  <ToolBar_ @validarAccion="validarAccion" />
+  <ConfigMaestros_
+    @guardar="config_maestro.estado = false"
+    :configuracion="config_maestro"
+    v-if="config_maestro.estado"
+    @cerrar="closePage"
+  />
 </template>
 
 <script setup>
-import { useApiContabilidad, useModuleCon851 } from "@/store";
-import { defineAsyncComponent } from "vue";
+import { defineAsyncComponent, ref } from "vue";
 
+const ConfigMaestros_ = defineAsyncComponent(() => import("@/components/consen/ConfigMaestros.vue"));
 const ConfigUsunet_ = defineAsyncComponent(() => import("@/components/consen/ConfigUsunet.vue"));
-const { logOut$ } = useApiContabilidad();
-const { CON851 } = useModuleCon851();
+const ToolBar_ = defineAsyncComponent(() => import("@/components/global/ToolBar.vue"));
+
+const config_maestro = ref({ estado: false });
+const estado = ref(true);
 
 const closePage = () => {
-  CON851("?", "success", "Error consultando datos paciente", logOut$);
+  setTimeout(() => {
+    config_maestro.value.estado = false;
+    estado.value = false;
+  }, 500);
+};
+
+const validarAccion = (event) => {
+  switch (event) {
+    case 0: // configuracion de servidor
+      return (estado.value = true);
+
+    case 1: // configuracion de maestros
+      return (config_maestro.value.estado = true);
+  }
 };
 </script>
