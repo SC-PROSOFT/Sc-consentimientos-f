08db5ebb50e8960aab680339ea61f1c41c896685
danielmendez2 <daniel_20-09@hotmail.com>
2025-03-10 10:01:32 -0500

    hic059

---
+++ src/impresiones/HIC/HIC059.js
@@ -0,0 +1,381 @@
+import { evaluarParentesco } from "@/formatos/utils";
+import { useModuleFormatos } from "@/store";
+
+const { getPaci } = useModuleFormatos();
+
+export const impresionHIC059 = ({ datos }) => {
+  console.log("datos ", datos);
+  var dd = {
+    stack: [contenidoHIC059(), firmas()],
+  };
+  function contenidoHIC059() {
+    return {
+      stack: [
+        {
+          marginTop: 10,
+          bold: true,
+          alignment: "center",
+          text: "CONSENTIMIENTO INFORMADO PARA ATENCIÓN PRIMARIA EN SALUD",
+          style: "bodyNoBold9",
+        },
+        {
+          marginTop: 10,
+          alignment: "justify",
+          text: [
+            { text: "Yo " },
+            { text: datos.paciente.descrip },
+            { text: ", identificado(a) con tipo de documento: " },
+            { text: datos.paciente.tipo_id },
+            { text: " , N°: " },
+            { text: datos.paciente.cod },
+            { text: " , expedida en " },
+            { text: datos.paciente.descrip_ciudad },
+            { text: " , residente del municipio " },
+            { text: datos.municipio },
+            { text: " , en el MICROTERRITORIO: " },
+            { text: datos.microterritorio },
+            { text: " , en representación de la familia: " },
+            { text: datos.familia.trim() },
+            { bold: true, text: " , Si autorizo: " },
+            { bold: true, decoration: "underline", text: datos.autoriza == "S" ? " X " : "    " },
+            { bold: true, text: " , No autorizo: " },
+            { bold: true, decoration: "underline", text: datos.autoriza == "N" ? " X " : "    " },
+            {
+              text: " , al equipo básico de salud del Plan de Intervenciones Colectivas PIC de la E.S.E. PRIMER NIVEL GRANADA SALUD, en específico al auxiliar de enfermería, enfermera, psicólogo, médico.",
+            },
+            {
+              text: "para que ingrese a mi hogar con el objetivo de desarrollar la Estrategia de Atención Primaria en Salud comprometiéndome a suministrar la información requerida mediante el diligenciamiento de fichas caracterización, y en las etapas de plan de cuidado y su respectivo cumplimiento.",
+            },
+          ],
+          style: "bodyNoBold9",
+        },
+        {
+          marginTop: 10,
+          text: "Me comprometo a cumplir con mis obligaciones tales como:",
+          style: "bodyNoBold9",
+        },
+        {
+          marginTop: 10,
+          style: "bodyNoBold9",
+          ul: [
+            "Estar presente en el domicilio la fecha indicada para la visita.",
+            "Tener un trato cordial y respetuoso con el personal de salud.",
+            "Suministrar información veraz para ser consignada en las fichas de caracterización ambiental, familiar e individual en el entorno hogar.",
+            "Revisar, concertar las actividades y las recomendaciones del plan integral de cuidado primario (PICP).",
+            "Cumplir con las citas asignadas por la IPS de referencia.",
+            "Participar activamente en los seguimientos y las actividades de intervenciones colectivas para cumplir con las tareas asignadas dentro PICP.",
+            "Preguntar cuando no entienda algo relacionado con mi salud y la de mi núcleo familiar.",
+            "Informar cambio de domicilio o número de teléfono al programa.",
+            "Acatar las indicaciones para la realización de los procedimientos de enfermería y psicología en casa.",
+          ],
+        },
+        {
+          marginTop: 10,
+          alignment: "justify",
+          text: [
+            { text: "Dado en: " },
+            { text: datos.lugar_atenc },
+            { text: "el día: " },
+            { text: datos.dia_atenc },
+            { text: " del mes de: " },
+            { text: datos.mes_atenc },
+            { text: " del año 2025." },
+          ],
+          style: "bodyNoBold9",
+        },
+        {
+          marginTop: 10,
+          alignment: "justify",
+          text: [
+            { text: "Autoriza registro fotográfico: SI" },
+            { decoration: "underline", text: datos.autoriza_foto == "S" ? " X " : "    " },
+            { text: " ; NO " },
+            { decoration: "underline", text: datos.autoriza_foto == "N" ? " X " : "    " },
+          ],
+          style: "bodyNoBold9",
+        },
+        {
+          marginTop: 10,
+          text: "En constancia firma",
+          style: "bodyNoBold9",
+        },
+        {
+          marginTop: 10,
+          style: "bodyNoBold9",
+          table: {
+            headerRows: 1,
+            widths: ["30%", "70%"],
+
+            body: [
+              ["Nombre completo", datos.paciente.descrip],
+              ["Tipo  y numero de documento", `${datos.paciente.tipo_id} ${datos.paciente.cod}`],
+              ["Teléfono", datos.telefono],
+              ["Dirección", datos.direcc],
+            ],
+          },
+        },
+      ],
+      styles: {
+        bodyContent: {
+          fontSize: 11,
+          alignment: "justify",
+        },
+      },
+    };
+  }
+  function firmaHuellaPaci(huella_paci, cant_firmas) {
+    let tamano_firma = 0;
+
+    if (cant_firmas == 2) {
+      tamano_firma = 105;
+    } else {
+      tamano_firma = 130;
+    }
+    const conHuella = {
+      marginLeft: 3,
+      columns: [
+        {
+          marginTop: 9,
+          alignment: "center",
+          image: "firma_paci",
+          width: tamano_firma,
+          height: 70,
+        },
+        {
+          marginTop: 9,
+          marginLeft: 5,
+          image: "huella_paci",
+          width: 55,
+          height: 70,
+        },
+      ],
+    };
+    const sinHuella = {
+      marginLeft: 3,
+      marginTop: 9,
+      alignment: "center",
+      image: "firma_paci",
+      width: tamano_firma,
+      height: 70,
+    };
+
+    if (huella_paci) return conHuella;
+    else return sinHuella;
+  }
+
+  function firmaPaciente(huella_paci, cant_firmas) {
+    return {
+      stack: [
+        {
+          text: "PACIENTE (FIRMA / HUELLA)",
+          bold: true,
+          alignment: "center",
+          style: "tableNoBold",
+        },
+        firmaHuellaPaci(huella_paci, cant_firmas),
+        {
+          marginTop: 10,
+          columns: [
+            {
+              width: "auto",
+              style: "tableNoBold",
+              text: "NOMBRE: ",
+              bold: true,
+            },
+            {
+              marginLeft: 5,
+              style: "tableNoBold",
+              text: `${datos.paciente.descrip}`,
+            },
+          ],
+        },
+        {
+          columns: [
+            {
+              width: "auto",
+              style: "tableNoBold",
+              text: "DOCUMENTO: ",
+              bold: true,
+            },
+            {
+              marginLeft: 5,
+              style: "tableNoBold",
+              text: `${datos.paciente.cod}`,
+            },
+          ],
+        },
+      ],
+    };
+  }
+  function firmaAcompanante() {
+    return {
+      stack: [
+        {
+          text: "TUTOR O ACOMPAÑANTE (FIRMA / HUELLA)",
+
+          alignment: "center",
+          style: "tableNoBold",
+          bold: true,
+        },
+        {
+          text: "(EN CASO DE NO FIRMAR)",
+          alignment: "center",
+          style: "tableNoBold",
+          fontSize: 6,
+        },
+        {
+          marginTop: 2,
+          alignment: "center",
+          image: "firma_acomp",
+          width: 130,
+          height: 70,
+        },
+        {
+          marginTop: 10,
+          columns: [
+            {
+              width: "auto",
+              style: "tableNoBold",
+              text: "NOMBRE: ",
+              bold: true,
+            },
+            {
+              marginLeft: 5,
+              style: "tableNoBold",
+              text: `${datos.acomp.descrip}`,
+            },
+          ],
+        },
+        {
+          columns: [
+            {
+              width: "auto",
+              style: "tableNoBold",
+              text: "DOCUMENTO: ",
+              bold: true,
+            },
+            {
+              marginLeft: 5,
+              style: "tableNoBold",
+              text: `${datos.acomp.cod}`,
+            },
+          ],
+        },
+        {
+          columns: [
+            {
+              width: "auto",
+              style: "tableNoBold",
+              text: "PARENTESCO: ",
+              bold: true,
+            },
+            {
+              marginLeft: 5,
+              style: "tableNoBold",
+              text: `${evaluarParentesco(datos.paren_acomp)}`,
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  function firmaProfesional() {
+    return {
+      stack: [
+        {
+          text: "FIRMA PROFESIONAL",
+
+          alignment: "center",
+          style: "tableNoBold",
+          bold: true,
+        },
+        {
+          marginTop: 8,
+          alignment: "center",
+          image: "firma_profesional",
+          width: 130,
+          height: 70,
+        },
+        {
+          marginTop: 8,
+          text: [
+            {
+              text: "NOMBRE: ",
+              style: "tableNoBold",
+              bold: true,
+            },
+            {
+              text: `${datos.prof.descrip}`,
+              style: "tableNoBold",
+            },
+          ],
+        },
+        {
+          columns: [
+            {
+              width: "auto",
+              style: "tableNoBold",
+              text: "PROFESIONAL AREA DE:",
+              bold: true,
+            },
+            {
+              marginLeft: 5,
+              style: "tableNoBold",
+              text: `${datos.prof.descrip_atiende}`,
+            },
+          ],
+        },
+        {
+          columns: [
+            {
+              width: "auto",
+              style: "tableNoBold",
+              text: "DOCUMENTO: ",
+              bold: true,
+            },
+            {
+              marginLeft: 5,
+              style: "tableNoBold",
+              text: `${datos.prof.cod}`,
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  function firmas() {
+    let firmasArray = [];
+    let anchos = ["40%"];
+    let tamanoFirmasArray = 0;
+
+    if (datos.firmas.firma_acomp) {
+      firmasArray.push(firmaAcompanante());
+    }
+
+    if (datos.firmas.firma_prof) {
+      firmasArray.push(firmaProfesional());
+    }
+
+    tamanoFirmasArray = firmasArray.length;
+
+    if (datos.firmas.firma_paci) {
+      firmasArray.unshift(firmaPaciente(datos.firmas.huella_paci, tamanoFirmasArray));
+    }
+
+    if (firmasArray.length == 2) {
+      firmasArray.unshift({ border: [false, false, false, false], text: "" });
+      anchos = ["10%", "40%", "40%"];
+    } else if (firmasArray.length == 3) anchos = ["33%", "34%", "33%"];
+    return {
+      marginTop: 30,
+      table: {
+        widths: anchos,
+        body: [[...firmasArray]],
+      },
+    };
+  }
+
+  return dd;
+};
---
+++ src/views/hic/HIC059.vue
@@ -0,0 +1,393 @@
+<template>
+  <q-card class="q-mx-auto format">
+    <div>
+      <q-card-section>
+        <div class="text-center">
+          <q-toggle
+            v-model="opcion_hic059"
+            color="primary"
+            keep-color
+            false-value="REVOCAR"
+            true-value="AUTORIZAR"
+            unchecked-icon="block"
+            checked-icon="check_circle"
+            label="¿Autorizar o revocar este consentimiento?"
+          />
+          <p :class="opcion_hic059 == 'AUTORIZAR' ? 'text-green' : 'text-red'">
+            <q-chip :color="opcion_hic059 == 'AUTORIZAR' ? 'green' : 'red'" class="text-white" v-if="opcion_hic059">
+              {{ opcion_hic059 }}
+            </q-chip>
+          </p>
+        </div>
+
+        <div class="row">
+          <p style="text-align: justify">
+            Yo <span class="text-bold">{{ getPaci.descrip }}</span> ,identificado (a) con tipo de documento:
+            <span class="text-bold">{{ getPaci.tipo_id }}</span
+            >N°: <span class="text-bold">{{ getPaci.cod }}</span> , expedida en
+          </p>
+          <div class="row q-mt-xs">
+            <p>
+              <span class="text-bold">{{ getPaci.descrip_ciudad }}</span> ,residente del municipio:
+            </p>
+            <Input_ v-model="HIC059.municipio" :field="form.municipio" :inputStyle="{ width: '482px' }" />
+          </div>
+
+          <div class="row q-mt-lg">
+            <p>en el MICROTERRITORIO:</p>
+            <Input_ v-model="HIC059.microterritorio" :field="form.microterritorio" :inputStyle="{ width: '581px' }" />
+          </div>
+          <div class="row q-mt-lg">
+            <p>en representación de la familia:</p>
+            <Input_ v-model="HIC059.familia" :field="form.familia" :inputStyle="{ width: '536px' }" />
+          </div>
+          <div class="row">
+            <div class="text-left">
+              <p style="font-weight: bold; margin-top: 10px">¿Autoriza?</p>
+            </div>
+            <div class="text-center">
+              <q-radio color="primary" v-model="HIC059.autoriza" val="S" label="SI" />
+              <q-radio color="primary" v-model="HIC059.autoriza" val="N" label="NO" />
+            </div>
+          </div>
+          <p>
+            al equipo básico de salud del Plan de Intervenciones Colectivas PIC de la E.S.E. PRIMER NIVEL GRANADA SALUD, en específico al auxiliar de
+            enfermería, enfermera, psicólogo, médico.
+          </p>
+        </div>
+
+        <div>
+          <p style="text-align: justify">
+            Para que ingrese a mi hogar con el objetivo de desarrollar la Estrategia de Atención Primaria en Salud comprometiéndome a suministrar la
+            información requerida mediante el diligenciamiento de fichas caracterización, y en las etapas de plan de cuidado y su respectivo
+            cumplimiento.
+          </p>
+          <p style="text-align: justify">Me comprometo a cumplir con mis obligaciones tales como:</p>
+          <ul>
+            <li>Estar presente en el domicilio la fecha indicada para la visita.</li>
+            <li>Tener un trato cordial y respetuoso con el personal de salud.</li>
+            <li>
+              Suministrar información veraz para ser consignada en las fichas de caracterización ambiental, familiar e individual en el entorno hogar.
+            </li>
+            <li>Revisar, concertar las actividades y las recomendaciones del plan integral de cuidado primario (PICP).</li>
+            <li>Cumplir con las citas asignadas por la IPS de referencia.</li>
+            <li>
+              Participar activamente en los seguimientos y las actividades de intervenciones colectivas para cumplir con las tareas asignadas dentro
+              PICP.
+            </li>
+            <li>Preguntar cuando no entienda algo relacionado con mi salud y la de mi núcleo familiar.</li>
+            <li>Informar cambio de domicilio o número de teléfono al programa.</li>
+            <li>Acatar las indicaciones para la realización de los procedimientos de enfermería y psicología en casa.</li>
+          </ul>
+        </div>
+        <div class="row">
+          <p style="text-align: justify">Dado en:</p>
+          <Input_ v-model="HIC059.lugar_atenc" :field="form.lugar_atenc" :inputStyle="{ width: '300px' }" />
+          <p style="text-align: justify">el dia:</p>
+          <Input_ v-model="HIC059.dia_atenc" :field="form.dia_atenc" :inputStyle="{ width: '40px' }" />
+          <p style="text-align: justify">del mes de:</p>
+          <Input_ v-model="HIC059.mes_atenc" :field="form.mes_atenc" :inputStyle="{ width: '150px' }" />
+          <p style="text-align: justify">del año 2025.</p>
+        </div>
+        <div class="row q-mt-lg">
+          <div class="text-left">
+            <p style="margin-top: 10px">¿Autoriza registro fotográfico?</p>
+          </div>
+          <div class="text-center">
+            <q-radio color="primary" v-model="HIC059.autoriza_foto" val="S" label="SI" />
+            <q-radio color="primary" v-model="HIC059.autoriza_foto" val="N" label="NO" />
+          </div>
+        </div>
+        <div class="row q-mt-xs">
+          <p style="text-align: justify">Teléfono:</p>
+          <Input_ v-model="HIC059.telefono" :field="form.telefono" :inputStyle="{ width: '150px' }" />
+        </div>
+        <div class="row q-mt-lg">
+          <p style="text-align: justify">Dirección:</p>
+          <Input_ v-model="HIC059.direcc" :field="form.direcc" :inputStyle="{ width: '750px' }" />
+        </div>
+      </q-card-section>
+    </div>
+    <q-separator />
+    <q-card-actions align="around" class="row">
+      <div class="col-12 row justify-around">
+        <ContainerFirma
+          quien_firma="FIRMA PACIENTE"
+          :firmador="getPaci.descrip"
+          :registro_profe="getPaci.cod"
+          @reciFirma="callBackFirma"
+          :huella_="huella_paci"
+          class="col-4"
+        />
+        <ContainerFirma
+          :firmador="getAcomp.descrip || 'NO HAY ACOMPAÑANTE'"
+          :disable="!getAcomp.descrip ? true : false"
+          quien_firma="FIRMA TUTOR O FAMILIAR"
+          :registro_profe="getAcomp.cod"
+          @reciFirma="callBackFirmaAcomp"
+          class="col-4"
+        />
+        <ContainerFirma
+          @reciFirma="callBackFirma"
+          :firma_="firma_prof"
+          :firmador="getProf.descrip"
+          :descrip_prof="getProf.descrip_atiende"
+          :registro_profe="getProf.registro_profe"
+          quien_firma="FIRMA PROFESIONAL"
+          class="col-4"
+        />
+      </div>
+    </q-card-actions>
+
+    <div class="col-12 row justify-center q-my-md">
+      <q-btn
+        :disable="opcion_hic059 ? false : true"
+        @click="validarDatos"
+        icon-right="check_circle"
+        class="q-mr-lg"
+        color="green"
+        label="GRABAR"
+        type="submit"
+      />
+    </div>
+  </q-card>
+</template>
+
+<script setup>
+import { useModuleFormatos, useApiContabilidad, useModuleCon851, useModuleCon851p } from "@/store";
+import { impresionHIC059, impresion, generarArchivo } from "@/impresiones";
+import { ref, defineAsyncComponent, onMounted, reactive } from "vue";
+import { calcularEdad, utilsFormat } from "@/formatos/utils";
+import { useRouter } from "vue-router";
+import { foco_ } from "@/setup";
+import dayjs from "dayjs";
+
+const ContainerFirma = defineAsyncComponent(() => import("@/components/global/containerFirma.vue"));
+const router = useRouter();
+
+const { getDll$, _getFirma$, _getHuella$, guardarFile$, enviarCorreo$, getEncabezado } = useApiContabilidad();
+const { getPaci, getAcomp, getHc, getProf, getEmpresa, getSesion } = useModuleFormatos();
+const { CON851P } = useModuleCon851p();
+const { CON851 } = useModuleCon851();
+
+const firma_recibida_acomp = ref("");
+const firma_recibida = ref("");
+const huella_paci = ref(null);
+const firma_prof = ref(null);
+const nit_usu = ref(parseInt(getEmpresa.nitusu) || 0);
+
+const HIC059 = reactive({
+  fecha_act: "",
+  municipio: "",
+  microterritorio: "",
+  familia: "",
+  autoriza: "",
+  lugar_atenc: "",
+  dia_atenc: "",
+  mes_atenc: "",
+  autoriza_foto: "",
+  telefono: "",
+  direcc: "",
+});
+const form = ref({
+  municipio: {
+    id: "municipio",
+    maxlength: "150",
+    label: "",
+    campo_abierto: true,
+  },
+  microterritorio: {
+    id: "microterritorio",
+    maxlength: "150",
+    label: "",
+    campo_abierto: true,
+  },
+  familia: {
+    id: "familia",
+    maxlength: "250",
+    label: "",
+    campo_abierto: true,
+  },
+  lugar_atenc: {
+    id: "lugar_atenc",
+    maxlength: "250",
+    label: "",
+    campo_abierto: true,
+  },
+  dia_atenc: {
+    id: "dia_atenc",
+    maxlength: "2",
+    label: "",
+    tipo: "number",
+    campo_abierto: true,
+  },
+  mes_atenc: {
+    id: "mes_atenc",
+    maxlength: "15",
+    label: "",
+    campo_abierto: true,
+  },
+  telefono: {
+    id: "telefono",
+    maxlength: "10",
+    label: "",
+    tipo: "number",
+    campo_abierto: true,
+  },
+  direcc: {
+    id: "direcc",
+    maxlength: "100",
+    label: "",
+    campo_abierto: true,
+  },
+});
+const opcion_hic059 = ref(null);
+
+onMounted(() => {
+  HIC059.fecha_act = dayjs(getEmpresa.fecha_act).format("YYYY-MM-DD");
+  getFirmaProf();
+});
+
+const getFirmaProf = async () => {
+  try {
+    firma_prof.value = await _getFirma$({ codigo: Number(getProf.cod) });
+    huella_paci.value = await _getHuella$({ codigo: getPaci.cod });
+  } catch (error) {
+    console.error(error);
+    CON851("?", "info", error);
+  }
+};
+
+const validarDatos = () => {
+  if (!firma_recibida.value && !getAcomp.cod) {
+    return CON851("?", "info", "No se ha realizado la firma del paciente");
+  }
+  if (getAcomp.cod && !firma_recibida_acomp.value) {
+    return CON851("?", "info", "No se ha realizado la firma del acompañante");
+  }
+
+  grabarConsentimiento();
+};
+
+const grabarConsentimiento = async () => {
+  const datos_format = JSON.parse(JSON.stringify(HIC059));
+  let datos = {
+    nit_entid: nit_usu.value,
+    estado: opcion_hic059.value == "AUTORIZAR" ? "1" : "2",
+    id_acomp: getAcomp.cod.padStart(15, "0"),
+    paren_acomp: getSesion.paren_acomp,
+    oper_consen: getSesion.oper,
+    llave_consen: getHc.llave,
+    cod_med: getProf.cod,
+    cod_consen: "HIC059",
+    disentimiento: "N",
+    ...datos_format,
+  };
+
+  getDll$({ modulo: `save_consen.dll`, data: { ...datos } })
+    .then((data) => {
+      if (data?.llave_consen) {
+        const fecha = data?.llave_consen.slice(23, 31);
+        return grabarFirmaConsen(data?.llave_consen);
+      }
+      CON851("?", "error", "Error al guardar el consentimiento");
+    })
+    .catch((error) => {
+      console.error(error);
+      CON851("?", "error", error);
+    });
+};
+
+const grabarFirmaConsen = async (llave) => {
+  try {
+    await guardarFile$({ base64: firma_recibida.value, codigo: `P${llave}` });
+    getAcomp.cod.trim() && (await guardarFile$({ base64: firma_recibida_acomp.value, codigo: `A${llave}` }));
+
+    if (getEmpresa.envio_email == "N") {
+      await imprimirConsen();
+      return router.back();
+    }
+    return CON851P(
+      "?",
+      "info",
+      "¿Deseas enviar el correo del consentimientos?",
+      async () => {
+        await imprimirConsen();
+        router.back();
+      },
+      async () => {
+        const file = await imprimirConsen();
+        if (getPaci.email && !/.+@.+\..+/.test(getPaci.email.toLowerCase())) {
+          return CON851("?", "info", "El correo no es valido", () => router.back());
+        }
+
+        const response = await enviarCorreo$({
+          cuerpo: `SE ADJUNTA ${getEncabezado.descrip} PARA ${getPaci.descrip} IDENTIDICADO CON ${getPaci.cod}`,
+          destino: getPaci.email.toLowerCase(),
+          subject: getEncabezado.descrip,
+          file,
+        });
+        CON851("?", response.tipo, response.message, () => router.back());
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    CON851("?", "info", error);
+  }
+};
+
+const imprimirConsen = async () => {
+  try {
+    const datos_hic059 = {
+      autorizo: opcion_hic059.value == "AUTORIZAR" ? true : false,
+      empresa: getEmpresa,
+      paciente: getPaci,
+      prof: getProf,
+      acomp: getAcomp,
+      paren_acomp: getSesion.paren_acomp,
+      firmas: {
+        firma_paci: firma_recibida.value ? true : false,
+        huella_paci: huella_paci.value ? true : false,
+        firma_acomp: firma_recibida_acomp.value ? true : false,
+        firma_prof: firma_prof.value ? true : false,
+      },
+      ...HIC059,
+    };
+
+    const firmas = {
+      img_firma_consen: firma_recibida.value,
+      img_firma_paci: firma_recibida.value,
+      img_huella_paci: huella_paci.value,
+      img_firma_acomp: firma_recibida_acomp.value,
+      firma_prof: firma_prof.value,
+    };
+
+    const docDefinitionPrint = await utilsFormat({
+      datos: { ...firmas, cod_consen: "HIC059" },
+      content: impresionHIC059({
+        datos: datos_hic059,
+      }),
+    });
+    const docDefinitionFile = await utilsFormat({
+      datos: { ...firmas, cod_consen: "HIC059" },
+      content: impresionHIC059({
+        datos: datos_hic059,
+      }),
+    });
+
+    await impresion({ docDefinition: docDefinitionPrint });
+    const response_impresion = await generarArchivo({ docDefinition: docDefinitionFile });
+    return response_impresion;
+  } catch (error) {
+    console.error("error -->", error);
+  }
+};
+
+const callBackFirma = (data_firma) => {
+  data_firma && (firma_recibida.value = data_firma);
+};
+
+const callBackFirmaAcomp = (data_firma) => {
+  data_firma && (firma_recibida_acomp.value = data_firma);
+};
+</script>
