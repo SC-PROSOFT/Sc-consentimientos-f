8884d57f8b7c3518937660ef0484eac5f0c8ab95
danielmendez2 <daniel_20-09@hotmail.com>
2025-03-12 10:48:38 -0500

    HIC067

---
+++ src/impresiones/HIC/HIC067.js
@@ -0,0 +1,338 @@
+import { evaluarParentesco } from "@/formatos/utils";
+import { useModuleFormatos } from "@/store";
+
+const { getPaci } = useModuleFormatos();
+
+export const impresionHIC067 = ({ datos }) => {
+  var dd = {
+    stack: [contenidoHIC067(), firmas()],
+  };
+
+  function contenidoHIC067() {
+    return {
+      stack: [
+        {
+          marginTop: 8,
+          text: [
+            { bold: true, style: "tableTitle", text: "Fecha: " },
+            { style: "bodyNoBold9", text: datos.fecha },
+          ],
+        },
+        {
+          marginTop: 8,
+          text: [
+            { bold: true, style: "tableTitle", text: " Lugar: " },
+            { style: "bodyNoBold9", text: datos.lugar },
+          ],
+        },
+        {
+          marginTop: 8,
+          alignment: "justify",
+          text: [
+            {
+              text: "Yo, ",
+            },
+            {
+              text: datos.paciente.descrip,
+              bold: true,
+            },
+            {
+              text: "con identificación: ",
+            },
+            {
+              text: datos.paciente.tipo_id,
+              bold: true,
+            },
+            {
+              text: "N° ",
+            },
+            {
+              text: datos.paciente.cod,
+              bold: true,
+            },
+            {
+              text: "de, ",
+            },
+            {
+              text: datos.paciente.descrip_ciudad,
+              bold: true,
+            },
+            {
+              text: ", Manifiesto que, de manera detallada se me han suministrado información completa y suficiente, con un lenguaje sencillo y claro a cerca de los pasos correspondiente para la obtención del certificado de discapacidad; por lo cual doy mi consentimiento para que el equipo multidisciplinario acceda a los registros de historia clínica y se me aplique la entrevista estructurada en el aplicativo nacional de la plataforma SISPRO en el cual quedará consignada la información pertinente para el proceso.",
+            },
+          ],
+          style: "bodyNoBold9",
+        },
+        {
+          marginTop: 8,
+          style: "bodyNoBold9",
+          text: "La certificación se realiza teniendo en cuenta los protocolos de bioseguridad de acuerdo a la contingencia de la pandemia de COVID-19.",
+        },
+      ],
+      styles: {
+        bodyContent: {
+          fontSize: 11,
+          alignment: "justify",
+        },
+      },
+    };
+  }
+
+  function firmaHuellaPaci(huella_paci, cant_firmas) {
+    let tamano_firma = 0;
+
+    if (cant_firmas == 2) {
+      tamano_firma = 105;
+    } else {
+      tamano_firma = 130;
+    }
+    const conHuella = {
+      marginLeft: 3,
+      columns: [
+        {
+          marginTop: 9,
+          alignment: "center",
+          image: "firma_paci",
+          width: tamano_firma,
+          height: 70,
+        },
+        {
+          marginTop: 9,
+          marginLeft: 5,
+          image: "huella_paci",
+          width: 55,
+          height: 70,
+        },
+      ],
+    };
+
+    const sinHuella = {
+      marginLeft: 3,
+      marginTop: 9,
+      alignment: "center",
+      image: "firma_paci",
+      width: tamano_firma,
+      height: 70,
+    };
+
+    if (huella_paci) return conHuella;
+    else return sinHuella;
+  }
+
+  function firmaPaciente(huella_paci, cant_firmas) {
+    return {
+      stack: [
+        {
+          text: "PACIENTE (FIRMA / HUELLA)",
+          bold: true,
+          alignment: "center",
+          style: "tableNoBold",
+        },
+        firmaHuellaPaci(huella_paci, cant_firmas),
+        {
+          marginTop: 10,
+          columns: [
+            {
+              width: "auto",
+              style: "tableNoBold",
+              text: "NOMBRE: ",
+              bold: true,
+            },
+            {
+              marginLeft: 5,
+              style: "tableNoBold",
+              text: `${datos.paciente.descrip}`,
+            },
+          ],
+        },
+        {
+          columns: [
+            {
+              width: "auto",
+              style: "tableNoBold",
+              text: "DOCUMENTO: ",
+              bold: true,
+            },
+            {
+              marginLeft: 5,
+              style: "tableNoBold",
+              text: `${datos.paciente.cod}`,
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  function firmaAcompanante() {
+    return {
+      stack: [
+        {
+          text: "TUTOR O ACOMPAÑANTE (FIRMA / HUELLA)",
+
+          alignment: "center",
+          style: "tableNoBold",
+          bold: true,
+        },
+        {
+          text: "(EN CASO DE NO FIRMAR)",
+          alignment: "center",
+          style: "tableNoBold",
+          fontSize: 6,
+        },
+        {
+          marginTop: 2,
+          alignment: "center",
+          image: "firma_acomp",
+          width: 130,
+          height: 70,
+        },
+        {
+          marginTop: 10,
+          columns: [
+            {
+              width: "auto",
+              style: "tableNoBold",
+              text: "NOMBRE: ",
+              bold: true,
+            },
+            {
+              marginLeft: 5,
+              style: "tableNoBold",
+              text: `${datos.acomp.descrip}`,
+            },
+          ],
+        },
+        {
+          columns: [
+            {
+              width: "auto",
+              style: "tableNoBold",
+              text: "DOCUMENTO: ",
+              bold: true,
+            },
+            {
+              marginLeft: 5,
+              style: "tableNoBold",
+              text: `${datos.acomp.cod}`,
+            },
+          ],
+        },
+        {
+          columns: [
+            {
+              width: "auto",
+              style: "tableNoBold",
+              text: "PARENTESCO: ",
+              bold: true,
+            },
+            {
+              marginLeft: 5,
+              style: "tableNoBold",
+              text: `${evaluarParentesco(datos.paren_acomp)}`,
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  function firmaProfesional() {
+    return {
+      stack: [
+        {
+          text: "FIRMA PROFESIONAL",
+
+          alignment: "center",
+          style: "tableNoBold",
+          bold: true,
+        },
+        {
+          marginTop: 8,
+          alignment: "center",
+          image: "firma_profesional",
+          width: 130,
+          height: 70,
+        },
+        {
+          marginTop: 8,
+          text: [
+            {
+              text: "NOMBRE: ",
+              style: "tableNoBold",
+              bold: true,
+            },
+            {
+              text: `${datos.prof.descrip}`,
+              style: "tableNoBold",
+            },
+          ],
+        },
+        {
+          columns: [
+            {
+              width: "auto",
+              style: "tableNoBold",
+              text: "PROFESIONAL AREA DE:",
+              bold: true,
+            },
+            {
+              marginLeft: 5,
+              style: "tableNoBold",
+              text: `${datos.prof.descrip_atiende}`,
+            },
+          ],
+        },
+        {
+          columns: [
+            {
+              width: "auto",
+              style: "tableNoBold",
+              text: "DOCUMENTO: ",
+              bold: true,
+            },
+            {
+              marginLeft: 5,
+              style: "tableNoBold",
+              text: `${datos.prof.cod}`,
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  function firmas() {
+    let firmasArray = [];
+    let anchos = ["40%"];
+    let tamanoFirmasArray = 0;
+
+    if (datos.firmas.firma_acomp) {
+      firmasArray.push(firmaAcompanante());
+    }
+
+    if (datos.firmas.firma_prof) {
+      firmasArray.push(firmaProfesional());
+    }
+
+    tamanoFirmasArray = firmasArray.length;
+
+    if (datos.firmas.firma_paci) {
+      firmasArray.unshift(firmaPaciente(datos.firmas.huella_paci, tamanoFirmasArray));
+    }
+
+    if (firmasArray.length == 2) {
+      firmasArray.unshift({ border: [false, false, false, false], text: "" });
+      anchos = ["10%", "40%", "40%"];
+    } else if (firmasArray.length == 3) anchos = ["33%", "34%", "33%"];
+    return {
+      marginTop: 30,
+      table: {
+        widths: anchos,
+        body: [[...firmasArray]],
+      },
+    };
+  }
+
+  return dd;
+};
